9e96a889e741db8fc6672b79be152d977a116fe9
baudneo <86508179+baudneo@users.noreply.github.com>
2024-01-07 18:27:59 -0700

    Remove use_safetensors and low_cpu_mem_usage

--- wyoming_faster_whisper/faster_whisper/transcribe.py
+++ wyoming_faster_whisper/faster_whisper/transcribe.py
@@ -64,8 +64,8 @@ class HuggingFaceModel(ASRModel):
         model = AutoModelForSpeechSeq2Seq.from_pretrained(
             model_id,
             torch_dtype=torch_dtype,
-            low_cpu_mem_usage=True,
-            use_safetensors=True,
+            # low_cpu_mem_usage=True,
+            # use_safetensors=True,
         )
         model.to(device)
 
